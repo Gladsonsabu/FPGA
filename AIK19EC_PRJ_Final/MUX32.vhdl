178cc852f3344a836718fe368afbaa9aec35f009
Gladsonsabu <67744114+Gladsonsabu@users.noreply.github.com>
2023-06-21 16:22:45 +0530

    Update MUX32.vhdl

--- AIK19EC_PRJ_Final/MUX32.vhdl
+++ AIK19EC_PRJ_Final/MUX32.vhdl
@@ -3,7 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity DualChannel32BitMux is
     port (
-        sel      : in  std_logic;
+        MUX_CLK   : in  std_logic;
+        MUX_RST   : in  std_logic;
+        sel       : in  std_logic;
         channelA0 : in  std_logic_vector(31 downto 0);
         channelA1 : in  std_logic_vector(31 downto 0);
         channelB0 : in  std_logic_vector(4 downto 0);
@@ -15,14 +17,19 @@ end entity DualChannel32BitMux;
 
 architecture behavioral of DualChannel32BitMux is
 begin
-    process (sel, channelA0, channelA1, channelB0, channelB1)
+    process (MUX_CLK, sel)
     begin
-        if sel = '0' then
-            outputA <= channelA0;
-            outputB <= channelB0;
-        else
-            outputA <= channelA1;
-            outputB <= channelB1;
+        if rising_edge(MUX_CLK)then
+            if MUX_RST = '1' then
+                outputA <= x"00000000";
+                outputB <= b"00000";
+            elsif sel = '0' then
+                outputA <= channelA0;
+                outputB <= channelB0;
+            else
+                outputA <= channelA1;
+                outputB <= channelB1;
+            end if;
         end if;
     end process;
 end architecture behavioral;
